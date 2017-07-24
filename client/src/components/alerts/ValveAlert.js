62fce48f45f511ad4969564ef3a56ca4092cbfdc
Zachary Klein <zak@silver-chalice.com>
2017-07-24 15:52:34 -0500

    Refactor (remove usages of bind, replace simple classes with functional components)

--- client/src/components/alerts/ValveAlert.js
+++ client/src/components/alerts/ValveAlert.js
@@ -1,54 +1,57 @@
-import React, { Component } from "react";
-import { Alert } from 'reactstrap';
+import React, {Component} from "react";
+import {Alert} from 'reactstrap';
 import Disconnect from '../icons/Disconnect';
 import Gauge from'../icons/Gauge';
 import {MdNotificationsOff} from 'react-icons/lib/md';
 import './Alerts.css';
 
 class ValveAlert extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            visible: true
-        };
+    this.state = {
+      visible: true
+    };
+  }
 
-        this.onDismiss = this.onDismiss.bind(this);
-    }
+  onDismiss = () => {
+    this.setState({visible: false});
+  };
 
-    onDismiss() {
-        this.setState({ visible: false });
-    }
-    render() {
-        return (
-                <Alert color={this.props.color === 'disabled' ? '' : this.props.color} className={ this.props.color === 'disabled' ? 'disabled' : ''} isOpen={this.state.visible}>
-                    { this.props.leftIcon && (
-                        <div className="alert-icon-left">
-                            {(() => {
-                                switch(this.props.leftIcon) {
-                                    case 'Disconnected':
-                                        return <Disconnect size="32" color={this.props.color === 'disabled' ? '#777' : 'white'} />;
-                                    case 'Gauge':
-                                        return<Gauge size="32"color={this.props.color ==='disabled' ? '#777' : 'white'}/>;
-                                    default:
-                                        return null;
-                                }
-                            })()}
-                        </div>
-                    ) }
-                    {/* this.props.alertContent */}
-                    <div className="alert-content">
-                        <strong>Valve {this.props.valveNumber}</strong><br />
-                        <span className="alert-details">{this.props.alertType}: {this.props.station} - {this.props.time}</span>
-                    </div>
-                    <div className="alert-icon-right">
-                        { this.props.rightIcon && (
-                            <MdNotificationsOff size={32} onClick={this.onDismiss}/>
-                        )}
-                    </div>
-                </Alert>
-        );
-    }
+  render() {
+
+    const {color, leftIcon, rightIcon, valveNumber, station, alertType, time} = this.props;
+
+    return (
+      <Alert color={color === 'disabled' ? '' : color} className={color === 'disabled' ? 'disabled' : ''}
+             isOpen={this.state.visible}>
+        {leftIcon && (
+          <div className="alert-icon-left">
+            {(() => {
+              switch (leftIcon) {
+                case 'Disconnected':
+                  return <Disconnect size="32" color={color === 'disabled' ? '#777' : 'white'}/>;
+                case 'Gauge':
+                  return <Gauge size="32" color={color === 'disabled' ? '#777' : 'white'}/>;
+                default:
+                  return null;
+              }
+            })()}
+          </div>
+        ) }
+        {/* this.props.alertContent */}
+        <div className="alert-content">
+          <strong>Valve {valveNumber}</strong><br />
+          <span className="alert-details">{alertType}: {station} - {time}</span>
+        </div>
+        <div className="alert-icon-right">
+          {rightIcon && (
+            <MdNotificationsOff size={32} onClick={this.onDismiss}/>
+          )}
+        </div>
+      </Alert>
+    );
+  }
 }
 
 export default ValveAlert;
