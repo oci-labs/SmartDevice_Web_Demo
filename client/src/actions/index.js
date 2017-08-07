9bdc9780e299219374e9f3eaca9643a870f44951
Ricky Harkins <rickyharkins@gmail.com>
2017-08-07 11:44:03 -0500

    Changing getValve url

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -10,8 +10,12 @@ function GETItem(item) {
   return fetch(`${SERVER_URL}/api/${item.type}/${item.id ? item.id : ""}`);
 }
 
-function GETValve(stationId) {
-  return fetch(`${SERVER_URL}/api/valve/station/${stationId}`);
+function GETValve(station) {
+  let url = new URL(`${SERVER_URL}/api/valve/station`),
+    params = { station: station.id, manifold: station.parent.id };
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
+
+  return fetch(url);
 }
 
 function GETValveStatus(valve) {
@@ -148,7 +152,7 @@ export function setSelectedItem(item, keepViewState) {
               break;
             case "station":
               dispatch(setSelectedStation(response));
-              dispatch(setValve(response.number));
+              dispatch(setValve(response));
               break;
             default:
               console.log("Not handled yet", response, item.type);
@@ -173,9 +177,9 @@ export function setSelectedItem(item, keepViewState) {
   };
 }
 
-function setValve(stationId) {
+function setValve(station) {
   return dispatch => {
-    return GETValve(stationId).then(toJson).then(response => {
+    return GETValve(station).then(toJson).then(response => {
       dispatch(setSelectedValve(response));
     });
   };
