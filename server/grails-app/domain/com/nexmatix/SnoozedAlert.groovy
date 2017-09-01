bbc036a14e1c820daef7f32a749d02c26ea3d0e2
Zachary Klein <zak@silver-chalice.com>
2017-09-01 12:18:40 -0700

    Handle composite keys across datasource

--- server/grails-app/domain/com/nexmatix/SnoozedAlert.groovy
+++ server/grails-app/domain/com/nexmatix/SnoozedAlert.groovy
@@ -6,10 +6,30 @@ import grails.rest.Resource
 class SnoozedAlert {
 
     User user
-    ValveAlert valveAlert
     Date snoozedAt
     Long duration = 120000l
 
+    Map<String, Integer> valveAlertId
+    private ValveAlert valveAlert
+
+    ValveAlert getValveAlert() {
+        if (!valveAlert && valveAlertId) {
+            valveAlert = ValveAlert.get(compositeKey(valveAlertId))
+        }
+        valveAlert
+    }
+
+    void setValveAlert(ValveAlert v) {
+        valveAlertId = [(v.alertType): v.valveSerialNumber]
+    }
+
+    static transients = ['valveAlert']
+
     static constraints = {
+    }
+
+    private static ValveAlert compositeKey(Map<String, Integer> key) {
+        new ValveAlert(alertType: key.keySet()[0],
+                valveSerialNumber: key.values()[0])
     }
 }
