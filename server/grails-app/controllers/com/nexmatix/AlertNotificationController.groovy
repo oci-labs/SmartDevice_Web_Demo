2edec7e25e773492d168846b3dbb53be322fc5ee
ajlevinson <aj.levinson@axceroglobal.com>
2017-09-07 13:15:14 -0500

    NEX-21 - Add in new service user (nexmatix-mvd@objectcomputing.com)

--- server/grails-app/controllers/com/nexmatix/AlertNotificationController.groovy
+++ server/grails-app/controllers/com/nexmatix/AlertNotificationController.groovy
@@ -1,8 +1,6 @@
 package com.nexmatix
 
 import grails.plugin.springsecurity.annotation.Secured
-import grails.rest.*
-import grails.converters.*
 
 @Secured(['permitAll'])
 class AlertNotificationController {
@@ -10,8 +8,8 @@ class AlertNotificationController {
     def send(Integer valveId) {
         log.info("Sending an alert for valveId: " + valveId)
         sendMail {
-            from "donotreply@test.com"
-            to "aj.levinson@axceroglobal.com"
+            from "nexmatix-mvd@objectcomputing.com"
+            to "levinsona@objectcomputing.com"
             subject "Alert received for valve ${valveId}"
             text "An alert has been created for valveId: ${valveId}"
         }
