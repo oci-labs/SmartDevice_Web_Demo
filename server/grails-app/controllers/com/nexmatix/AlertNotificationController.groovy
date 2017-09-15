c93c47245f8940773fb0f509eaeb575858db4cc0
ajlevinson <aj.levinson@axceroglobal.com>
2017-09-15 11:01:16 -0500

    NEX-21 - Revert previous changes to reflect the Valve SN being passed in to trigger an email.

--- server/grails-app/controllers/com/nexmatix/AlertNotificationController.groovy
+++ server/grails-app/controllers/com/nexmatix/AlertNotificationController.groovy
@@ -4,21 +4,21 @@ class AlertNotificationController {
 
     def mailService
 
-    def send(Integer stationNum) {
-        println "Sending an alert for stationNum: " + stationNum
+    def send(Integer valveSN) {
+        println "Sending an alert for Valve SN: " + valveSN
         try {
             mailService.sendMail {
                 from "nexmatix-mvd@objectcomputing.com"
                 to "levinsona@objectcomputing.com"
-                subject "Alert received for station ${stationNum}"
-                text "An alert has been created for stationNum: ${stationNum}"
+                subject "Alert received for Valve SN ${valveSN}"
+                text "An alert has been created for Valve SN: ${valveSN}"
             }
             println "Sent email to levinsona@objectcomputing.com"
 
             render status: 200
         }
         catch (Exception e) {
-            println "Exception encountered sending email for stationNum", e
+            println "Exception encountered sending email for Valve SN: " + ${valveSN}, e
             render status: 500
         }
     }
