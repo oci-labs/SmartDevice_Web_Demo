2844afb18c5e5baedb44fd76a80c4e9a53c59602
Michael Kimberlin <mkimberlin@gmail.com>
2017-09-26 16:30:31 -0500

    Some eslint config 'fixes' and add a viewUserModal value to state along with a toggle action

--- client/src/redux-modules/view/reducer.spec.js
+++ client/src/redux-modules/view/reducer.spec.js
@@ -1,38 +1,63 @@
 /* global describe, it, expect */
 
 import {
-    setViewState, goToPreviousViewState, toggleAlerts, toggleProfile
+  setViewState, goToPreviousViewState, toggleAlerts, toggleProfile,
+  toggleUserModal
 } from './actions';
 import randomize from 'randomatic';
 import reducer, { initialState } from './reducer';
 
 describe('Selected context reducer', () => {
-    it('should set the department to the payload when a SET_VIEW_STATE action is received', () => {
-        const testItem = { foo: randomize('*', 10) };
-        const newState = reducer(initialState, setViewState(testItem));
-        const expectedState = { ...initialState, VIEW_STATE: testItem, PREVIOUS_VIEW_STATE: initialState.VIEW_STATE };
+  it('should set the view state to the payload and the set the previous view state when a SET_VIEW_STATE action is received', () => {
+    const testItem = { foo: randomize('*', 10) };
+    const newState = reducer(initialState, setViewState(testItem));
+    const expectedState = {
+      ...initialState,
+      VIEW_STATE: testItem,
+      PREVIOUS_VIEW_STATE: initialState.VIEW_STATE
+    };
 
-        expect(newState).toEqual(expectedState);
-    });
+    expect(newState).toEqual(expectedState);
+  });
 
-    it('should set the facility to the payload when a GO_TO_PREVIOUS_VIEW_STATE action is received', () => {
-        const newState = reducer(initialState, goToPreviousViewState());
-        const expectedState = { ...initialState, VIEW_STATE: initialState.PREVIOUS_VIEW_STATE, PREVIOUS_VIEW_STATE: initialState.VIEW_STATE };
+  it('should set the view state to the previous view state and the previous to the view state when a GO_TO_PREVIOUS_VIEW_STATE action is received', () => {
+    const newState = reducer(initialState, goToPreviousViewState());
+    const expectedState = {
+      ...initialState,
+      VIEW_STATE: initialState.PREVIOUS_VIEW_STATE,
+      PREVIOUS_VIEW_STATE: initialState.VIEW_STATE
+    };
 
-        expect(newState).toEqual(expectedState);
-    });
+    expect(newState).toEqual(expectedState);
+  });
 
-    it('should set the machine to the payload when a TOGGLE_PROFILE action is received', () => {
-        const newState = reducer(initialState, toggleProfile());
-        const expectedState = { ...initialState, viewProfile: !initialState.viewProfile };
+  it('should toggle the value of viewProfile when a TOGGLE_PROFILE action is received', () => {
+    const newState = reducer(initialState, toggleProfile());
+    const expectedState = {
+      ...initialState,
+      viewProfile: !initialState.viewProfile
+    };
 
-        expect(newState).toEqual(expectedState);
-    });
+    expect(newState).toEqual(expectedState);
+  });
 
-    it('should set the manifold to the payload when a TOGGLE_ALERTS action is received', () => {
-        const newState = reducer(initialState, toggleAlerts());
-        const expectedState = { ...initialState, viewAlerts: !initialState.viewAlerts };
+  it('should toggle the value of viewAlerts when a TOGGLE_ALERTS action is received', () => {
+    const newState = reducer(initialState, toggleAlerts());
+    const expectedState = {
+      ...initialState,
+      viewAlerts: !initialState.viewAlerts
+    };
 
-        expect(newState).toEqual(expectedState);
-    });
+    expect(newState).toEqual(expectedState);
+  });
+
+  it('should toggle the value of showUserModal when a TOGGLE_USER_MODAL action is received', () => {
+    const newState = reducer(initialState, toggleUserModal());
+    const expectedState = {
+      ...initialState,
+      showUserModal: !initialState.showUserModal
+    };
+
+    expect(newState).toEqual(expectedState);
+  });
 });
