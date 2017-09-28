85ac030c4e4267dcd3776b028c4fbeefae3bf0d9
Michael Kimberlin <mkimberlin@gmail.com>
2017-09-28 17:37:50 -0500

    Add tests to add user component and refactor to out of container

---
+++ client/src/setupTests.js
@@ -0,0 +1,20 @@
+/* global expect */
+import Adapter from 'enzyme-adapter-react-15';
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import toJson, { shallowToJson } from 'enzyme-to-json';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.shallowToJson = shallowToJson;
+global.snapshot = component => expect(shallowToJson(shallow(component))).toMatchSnapshot();
+global.renderedSnapshot = (component, context) =>
+  expect(toJson(render(component, context))).toMatchSnapshot();
+
+// dummy mock for window.matchMedia which JSDom doesn't support
+if (global.window) {
+  global.window.matchMedia = () => ({ matches: true });
+}
